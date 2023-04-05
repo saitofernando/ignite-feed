b04d847c9c0ce146e1e0497b75db57b2a38a3072
saitofernando <fernandosaito@hotmail.com.br>
2023-04-05 00:14:51 -0400

    create component Comment and your style module

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/76966991?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Fernando Saito</strong>
+
+              <time  title="28 de Fevereiro às 00:07" dateTime="2023-02-28 00:07">
+                Cerca de 1 hora atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!</p>
+        </div>
+      
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post(props) {
@@ -42,6 +43,13 @@ export function Post(props) {
       
           </form>
 
+          <div className={styles.commentList}>
+            <Comment />
+            <Comment />
+            <Comment />
+
+
+          </div>
       </article>
     )
 }
