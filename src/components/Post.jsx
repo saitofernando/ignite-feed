7d93f903f6ccf6416826c99aba5818acf0f117c6
Fernando Saito <fernandosaito@hotmail.com.br>
2024-04-14 18:31:50 -0400

    validating forms

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,7 +32,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório.')
   }
 
   function deleteComment(commentToDelete) {
@@ -43,6 +48,8 @@ export function Post({ author, publishedAt, content }) {
     setComments(commentsWithoutDeletedOne);
 
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -85,11 +92,13 @@ export function Post({ author, publishedAt, content }) {
           name="comment"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
           required
+          
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
         </footer>
       </form>
 
