af722acfc600c44d1ac584c1ac53ea1d64f17aab
Fernando Saito <fernandosaito@hotmail.com.br>
2024-04-14 19:18:23 -0400

    feature: like comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,20 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
 
 export function Comment({ content, onDeleteComment }) {
+
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment(){
     onDeleteComment(content)
   }
    
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/maykbrito.png" />
@@ -36,9 +42,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
